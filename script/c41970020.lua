4c34bcdd385b89028677c0b4aca3c3261f2fb1fe
KSB-Custom <124850393+KSB-Custom@users.noreply.github.com>
2023-02-13 08:50:21 -0600

    Add files via upload

--- script/c41970020.lua
+++ script/c41970020.lua
@@ -68,7 +68,7 @@ end
 function s.copyop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
-	if tc and c:IsRelateToEffect(e) and c:IsFaceup() and tc:IsRelateToEffect(e) and tc:IsLocation(LOCATION_GRAVE)
+	if tc and c:IsRelateToEffect(e) and c:IsFaceup() and tc:IsRelateToEffect(e) and tc:IsLocation(LOCATION_GRAVE) then
 		local code=tc:GetOriginalCode()
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
