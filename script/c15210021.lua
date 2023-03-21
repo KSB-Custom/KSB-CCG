f720e4ecba8f86a429b8059d3ad77c035a27aa37
KSB-Custom <124850393+KSB-Custom@users.noreply.github.com>
2023-03-21 16:22:50 -0600

    Add files via upload

--- script/c15210021.lua
+++ script/c15210021.lua
@@ -4,6 +4,7 @@ function s.initial_effect(c)
 --Synchro summon
 	Synchro.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsSetCard,0x5F1),1,1,Synchro.NonTuner(nil),1,99)
 	c:EnableReviveLimit()
+	--Op cannot be target
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
@@ -11,7 +12,7 @@ function s.initial_effect(c)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetTargetRange(LOCATION_MZONE,0)
 	e1:SetTarget(s.indtg)
-	e1:SetValue(1)
+	e1:SetValue(tgoval)
 	c:RegisterEffect(e1)
 	--Disable attack
 	local e2=Effect.CreateEffect(c)
