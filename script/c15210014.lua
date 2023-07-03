e20090e29c20af20c2b650ede42b5d5005dd3d9c
KSB-Custom <124850393+KSB-Custom@users.noreply.github.com>
2023-07-03 11:46:55 -0600

    Add files via upload

--- script/c15210014.lua
+++ script/c15210014.lua
@@ -1,7 +1,7 @@
 --AZTECA Eagle warrior
 local s,id=GetID()
 function s.initial_effect(c)
-	--spsummon
+	--spsummon tokens
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOKEN)
@@ -35,7 +35,24 @@ function s.top(e,tp,eg,ep,ev,re,r,rp)
 		or not Duel.IsPlayerCanSpecialSummonMonster(tp,id-4,0x5F1,TYPES_TOKEN,100,100,3,RACE_PLANT,ATTRIBUTE_EARTH,POS_FACEUP) then return end
 	local t1=Duel.CreateToken(tp,id-4)
 	local t2=Duel.CreateToken(tp,id-4)
+	local token=t1,t2
 		Duel.SpecialSummonStep(t1,0x5F1,tp,tp,false,false,POS_FACEUP)
 		Duel.SpecialSummonStep(t2,0x5F1,tp,tp,false,false,POS_FACEUP)
-	Duel.SpecialSummonComplete()
+	local e1=Effect.CreateEffect(t1)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_CANNOT_BE_LINK_MATERIAL)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e1:SetValue(s.synlimit)
+	t1:RegisterEffect(e1,true)
+	local e2=Effect.CreateEffect(t2)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_CANNOT_BE_LINK_MATERIAL)
+	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e2:SetValue(s.synlimit)
+	t2:RegisterEffect(e2,true)
+		Duel.SpecialSummonComplete()
+end
+function s.synlimit(e,c)
+	if not c then return false end
+	return not c:IsSetCard(0x5F1)
 end
