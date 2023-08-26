a67a62340fc8e329ffddf19d19d6748a22571943
KSB-Custom <124850393+KSB-Custom@users.noreply.github.com>
2023-08-26 01:14:23 -0600

    Add files via upload

--- script/c41970009.lua
+++ script/c41970009.lua
@@ -13,19 +13,19 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 	--Can be activated from the hand
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetType(EFFECT_TYPE_SINGLE)
 	e2:SetCode(EFFECT_TRAP_ACT_IN_HAND)
 	e2:SetCondition(s.handcon)
 	c:RegisterEffect(e2)
 	--Add itself to the hand
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetDescription(aux.Stringid(id,2))
 	e3:SetCategory(CATEGORY_TOHAND)
 	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e3:SetCode(EVENT_TO_GRAVE)
+	e3:SetCode(EVENT_DISCARD)
 	e3:SetRange(LOCATION_GRAVE)
-	e3:SetCountLimit(2,{id,1})
+	e3:SetCountLimit(1,{id,1})
 	e3:SetCondition(s.thcon)
 	e3:SetTarget(s.thtg)
 	e3:SetOperation(s.thop)
@@ -34,7 +34,7 @@ end
 s.listed_series={0x1065}
 --Special summon
 function s.spfilter(c,e,tp)
-	return c:IsSetCard(0x1065) or (c:IsFaceup() and c:IsSetCard(0x1065) and c:IsType(TYPE_PENDULUM))) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	return c:IsSetCard(0x1065) or ((c:IsFaceup() and c:IsSetCard(0x1065)) and c:IsType(TYPE_PENDULUM)) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
@@ -52,24 +52,21 @@ end
 --Condition activation in hand
 function s.handcon(e)
 	return Duel.GetFieldGroupCount(e:GetHandlerPlayer(),LOCATION_ONFIELD,0)==0
-		and Duel.GetFieldGroupCount(tp,0,LOCATION_MZONE)>1
+		and Duel.GetFieldGroupCount(1-e:GetHandlerPlayer(),LOCATION_ONFIELD,0)>1
 end
 --Add itself to the hand
-function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local c=e:GetHandler()
-	if chk==0 then return c:IsAbleToHand() end
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,c,1,0,0)
-	Duel.SetOperationInfo(0,CATEGORY_LEAVE_GRAVE,c,1,0,0)
-end
-function s.thop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	if c:IsRelateToEffect(e) then
-		Duel.SendtoHand(c,nil,REASON_EFFECT)
-	end
-end
 function s.thcfilter(c,tp)
-	return c:GetPreviousLocation()==LOCATION_HAND and c:IsSetCard(0x1065) and c:IsReason(REASON_DISCARD)
+	return c:IsControler(tp) and c:IsPreviousControler(tp) and c:IsSetCard(0x1065) and c:IsReason(REASON_EFFECT)
 end
 function s.thcon(e,tp,eg,ep,ev,re,r,rp)
-	return eg and eg:IsExists(s.thcfilter,1,e:GetHandler(),tp)
+	return eg:IsExists(s.thcfilter,1,nil,tp)
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsAbleToHand() end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,e:GetHandler(),1,0,0)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetHandler():IsRelateToEffect(e) then
+		Duel.SendtoHand(e:GetHandler(),nil,REASON_EFFECT)
+	end
 end
