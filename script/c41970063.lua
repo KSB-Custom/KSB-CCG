fc71d12d553893deb4bebf92f6474fdfaaa36c72
KSB-Custom <124850393+KSB-Custom@users.noreply.github.com>
2023-09-04 21:35:08 -0600

    Add files via upload

--- script/c41970063.lua
+++ script/c41970063.lua
@@ -1,55 +1,35 @@
---RPG Tank X Berseker
+--FNO Party
 local s,id=GetID()
 function s.initial_effect(c)
-	--Xyz summon
-	Xyz.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsSetCard,0x1065),8,2,nil,nil,99)
-	c:EnableReviveLimit()
-	--pendulum summon
-	Pendulum.AddProcedure(c,false)
-	--cannot select battle target
+	Xyz.AddProcedure(c,nil,8,2,nil,nil,99)
+	--ritual material
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_EXTRA_RITUAL_MATERIAL)
 	e1:SetRange(LOCATION_MZONE)
-	e1:SetTargetRange(0,LOCATION_MZONE)
-	e1:SetCode(EFFECT_CANNOT_SELECT_BATTLE_TARGET)
-	e1:SetValue(function(e,c) return c~=e:GetHandler() end)
+	e1:SetProperty(EFFECT_FLAG_IGNORE_RANGE)
+	e1:SetTarget(s.mttg1)
+	e1:SetValue(1)
 	c:RegisterEffect(e1)
-	--cannot be targeted
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_SINGLE)
-	e2:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
-	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e2:SetRange(LOCATION_MZONE)
-	e2:SetValue(aux.tgoval)
-	c:RegisterEffect(e2)
-	--immune during Battle Phase
+	--PZone fusion material
 	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_SINGLE)
-	e3:SetCode(EFFECT_IMMUNE_EFFECT)
-	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetCode(EFFECT_EXTRA_FUSION_MATERIAL)
 	e3:SetRange(LOCATION_MZONE)
-	e3:SetCondition(s.immcon)
-	e3:SetValue(s.immval)
+	e3:SetTargetRange(LOCATION_PZONE,0)
+	e3:SetTarget(s.mttg2)
+	e3:SetValue(s.mtval)
 	c:RegisterEffect(e3)
-	--atk/def
-	local e4=Effect.CreateEffect(c)
-	e4:SetType(EFFECT_TYPE_SINGLE)
-	e4:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e4:SetRange(LOCATION_MZONE)
-	e4:SetCode(EFFECT_UPDATE_ATTACK)
-	e4:SetValue(s.atkval)
-	c:RegisterEffect(e4)
-	local e5=e4:Clone()
-	e5:SetCode(EFFECT_UPDATE_DEFENSE)
-	c:RegisterEffect(e5)
 	end
-	s.pendulum_level=8
-function s.immcon(e)
-	return Duel.IsBattlePhase()
+--Ritual
+function s.mttg1(e,c)
+	return e:GetHandler():GetOverlayGroup():IsContains(c)
 end
-function s.immval(e,te)
-	return te:GetOwnerPlayer()~=e:GetHandlerPlayer()
+--fusion
+function s.mttg2(e,c)
+	return e:GetHandler()
 end
-function s.atkval(e,c)
-	return c:GetOverlayCount()*500
+function s.mtval(e,c)
+	if not c then return false end
+	return c:IsSetCard(0x1065) and c:IsControler(e:GetHandlerPlayer())
 end
