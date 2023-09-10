4e1df38e1390c06a7d42a6bc42129e48f232d3ad
KSB-Custom <124850393+KSB-Custom@users.noreply.github.com>
2023-09-09 18:31:49 -0600

    Add files via upload

--- script/c41970053.lua
+++ script/c41970053.lua
@@ -31,7 +31,7 @@ function s.initial_effect(c)
 	e4:SetTarget(s.atktg)
 	e4:SetOperation(s.atkop)
 	c:RegisterEffect(e4)
-	--pierce
+	--add pierce eff
 	local e5=Effect.CreateEffect(c)
 	e5:SetType(EFFECT_TYPE_FIELD)
 	e5:SetCode(EFFECT_PIERCE)
