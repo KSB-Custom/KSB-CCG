a4aee7aee91720728dfa0fc4772c0ba6714b4d99
KSB-Custom <124850393+KSB-Custom@users.noreply.github.com>
2023-09-23 15:01:30 -0600

    Delete script/c31000003.lua

--- script/c31000003.lua
+++
@@ -1,9 +0,0 @@
---Majespecter Baby Unicorn 
-	--cannot target
-	local e4=Effect.CreateEffect(c)
-	e4:SetType(EFFECT_TYPE_SINGLE)
-	e4:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
-	e4:SetRange(LOCATION_MZONE)
-	e4:SetTargetRange(LOCATION_PZONE,LOCATION_PZONE)
-	e4:SetValue(aux.tgoval)
-	c:RegisterEffect(e4)
