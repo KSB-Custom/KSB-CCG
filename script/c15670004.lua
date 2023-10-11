8c9d258d23e8a4199a423283a1d6c93a3b69e41e
KSB-Custom <124850393+KSB-Custom@users.noreply.github.com>
2023-10-11 11:24:07 -0600

    Add files via upload

--- script/c15670004.lua
+++ script/c15670004.lua
@@ -88,12 +88,5 @@ function s.setop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
 	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil)
 	local tc=g:GetFirst()
-	if tc then
 		Duel.SSet(tp,tc)
-		local e1=Effect.CreateEffect(e:GetHandler())
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_CANNOT_TRIGGER)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD_EXC_GRAVE+RESET_PHASE+PHASE_END)
-		tc:RegisterEffect(e1)
-	end
 end
