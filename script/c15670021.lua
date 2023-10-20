ba44a8fde11e38368eec18a4e7e79462dfe32878
KSB-Custom <124850393+KSB-Custom@users.noreply.github.com>
2023-10-20 14:30:56 -0600

    Add files via upload

--- script/c15670021.lua
+++ script/c15670021.lua
@@ -3,8 +3,12 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--fusion material
 	c:EnableReviveLimit()
-	Fusion.AddProcMixN(c,true,true,s.ffilter,2)
+	Fusion.AddProcMixN(c,true,true,s.ffilter1,1,s.ffilter2,1)
 	end
-function s.ffilter(c,fc,sumtype,sp,sub,mg,sg)
-	return not sg or sg:FilterCount(aux.TRUE,c)==0 or not (sg:IsExists(Card.IsAttribute,1,c,c:GetAttribute(),fc,sumtype,sp))
+	s.listed_series={0xf16}
+function s.ffilter1(c,fc,sumtype,tp)
+	return c:IsSetCard(0xf16,fc,sumtype,tp) or (c:IsLocation(LOCATION_MZONE) and c:IsType(TYPE_EFFECT) and c:IsStatus(STATUS_SPSUMMON_TURN))
+end
+function s.ffilter2(c,fc,sumtype,tp)
+	return c:IsType(TYPE_EFFECT,fc,sumtype,tp) and c:IsType(TYPE_MONSTER,fc,sumtype,tp)
 end
