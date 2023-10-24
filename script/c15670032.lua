e66dab6a6195b873d215edab8f18a51316604056
KSB-Custom <124850393+KSB-Custom@users.noreply.github.com>
2023-10-24 14:27:52 -0600

    Add files via upload

--- script/c15670032.lua
+++ script/c15670032.lua
@@ -3,6 +3,9 @@ local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
 	--Synchro procedure
-	Synchro.AddProcedure(c,{aux.FilterBoolFunctionEx(Card.IsSetCard,0xf16),aux.FilterBoolFunctionEx(Card.IsSetCard,0xf15)},1,99,Synchro.NonTuner(nil),1,99)
+	Synchro.AddProcedure(c,s.sfilter,1,99,Synchro.NonTuner(nil),1,99)
 	end
-	
+	s.material_setcode={0xf16,0xf15}
+function s.sfilter(c)
+	return c:IsSetCard(0xf15) or c:IsSetCard(0xf16)
+end
