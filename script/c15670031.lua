3b784d3ae75725baa18686cfa6920311566bf268
KSB-Custom <124850393+KSB-Custom@users.noreply.github.com>
2023-10-25 18:51:47 -0600

    Add files via upload

--- script/c15670031.lua
+++ script/c15670031.lua
@@ -5,4 +5,42 @@ function s.initial_effect(c)
 	c:EnableReviveLimit()
 	--Synchro procedure
 	Synchro.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsSetCard,0xf16),1,99,Synchro.NonTuner(nil),1,1)
+	--search
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
 	end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsReleasable() end
+	Duel.Release(e:GetHandler(),REASON_COST)
+end
+function s.filter1(c)
+	return c:IsCode(CARD_POLYMERIZATION) and c:IsFaceup() and c:IsAbleToHand()
+end
+function s.filter2(c)
+	return c:IsSetCard(0xf16) and c:IsMonster() and c:IsFaceup() and c:IsAbleToHand()
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter1,tp,LOCATION_DECK,0,1,nil)
+		and Duel.IsExistingMatchingCard(s.filter2,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,2,tp,LOCATION_GRAVE+LOCATION_REMOVED)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local g1=Duel.GetMatchingGroup(s.filter1,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,nil)
+	local g2=Duel.GetMatchingGroup(s.filter2,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,nil)
+	if #g1>0 and #g2>0 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local sg1=g1:Select(tp,1,1,nil)
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local sg2=g2:Select(tp,1,1,nil)
+		sg1:Merge(sg2)
+		Duel.SendtoHand(sg1,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,sg1)
+	end
+end
