d1b9096e2a84203ea5f83571ef7a6cacdc7354ed
KSB-Custom <124850393+KSB-Custom@users.noreply.github.com>
2023-12-23 23:00:20 -0600

    Add files via upload

--- script/c15579013.lua
+++ script/c15579013.lua
@@ -3,7 +3,7 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
-	Synchro.AddProcedure(c,nil,1,1,Synchro.NonTuner(nil),1,99)
+	Synchro.AddProcedure(c,nil,1,1,Synchro.NonTuner(Card.IsSetCard,0x759),1,99)
 		--cannot be target
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
