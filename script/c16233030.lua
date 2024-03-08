49faa94a3d9eb4a24f08781e7e3a9a92d7a435e3
KSB-Custom <124850393+KSB-Custom@users.noreply.github.com>
2024-03-08 15:55:07 -0600

    Add files via upload

--- script/c16233030.lua
+++ script/c16233030.lua
@@ -21,7 +21,7 @@ function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():IsSummonType(SUMMON_TYPE_SYNCHRO)
 end
 function s.costfilter(c)
-	return c:GetLevel()<6 and c:IsDiscardable()
+	return c:GetLevel()<6 and c:IsSetCard(0xf19) and c:IsDiscardable()
 end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_HAND,0,1,nil) end
