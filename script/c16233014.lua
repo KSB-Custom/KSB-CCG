01d7986ac892ef548f08f414521db1bb740fe304
KSB-Custom <124850393+KSB-Custom@users.noreply.github.com>
2024-03-13 20:49:47 -0600

    Add files via upload

--- script/c16233014.lua
+++ script/c16233014.lua
@@ -3,6 +3,44 @@ local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
 	Synchro.AddProcedure(c,nil,1,1,nil,1,1)
-
+	--add counter
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_COUNTER)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetCountLimit(1)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+	--cannot attack announce
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e3:SetCode(EFFECT_CANNOT_ATTACK_ANNOUNCE)
+	e3:SetTarget(s.atktg)
+	c:RegisterEffect(e3)
 	end
-	
+	s.counter_place_list={0xf19}
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(1-tp) and chkc:IsCanAddCounter(0xf19,1) end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsCanAddCounter,tp,0,LOCATION_MZONE,1,nil,0xf19,1) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	local g=Duel.SelectTarget(tp,Card.IsCanAddCounter,tp,0,LOCATION_MZONE,1,1,nil,0xf19,1)
+	Duel.SetOperationInfo(0,CATEGORY_COUNTER,nil,1,0,0)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and tc:IsCanAddCounter(0xf19,1) then
+		local atk=tc:GetAttack()
+		tc:AddCounter(0xf19,1)
+		if atk>0 and tc:GetAttack()==0 then
+			Duel.RaiseEvent(tc,EVENT_CUSTOM+54306223,e,0,0,0,0)
+		end
+	end
+end
+function s.atktg(e,c)
+	return c:GetCounter(0xf19)>0
+end
