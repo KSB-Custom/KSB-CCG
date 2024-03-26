73cd28248e2372afb5d44ca09b9ca5f29a5b9d7b
KSB-Custom <124850393+KSB-Custom@users.noreply.github.com>
2024-03-26 01:33:59 -0600

    Add files via upload

--- script/c41971031.lua
+++ script/c41971031.lua
@@ -44,7 +44,7 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetCode(EFFECT_UPDATE_ATTACK)
 		e1:SetValue(-400)
 		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_BATTLE)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
 		tc:RegisterEffect(e1)
 	end
 end
