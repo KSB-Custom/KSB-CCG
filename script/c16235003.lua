8113956bda91caeaf656612dff0f38e6ed2d65c3
KSB-Custom <124850393+KSB-Custom@users.noreply.github.com>
2024-04-21 00:34:09 -0600

    Add files via upload

--- script/c16235003.lua
+++ script/c16235003.lua
@@ -54,6 +54,7 @@ function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
 end
 function s.atkop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
+	local atk=Duel.GetMatchingGroupCount(Card.IsMonster,0,LOCATION_GRAVE,LOCATION_GRAVE,nil)
 	if atk>0 and c:IsFaceup() and c:IsRelateToEffect(e) then
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
