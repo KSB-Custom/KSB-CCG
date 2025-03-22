eae69629074a821668a0671ea75078658117aecd
KSB-Custom <124850393+KSB-Custom@users.noreply.github.com>
2025-03-22 14:47:36 -0600

    Add files via upload

--- script/c41971064.lua
+++ script/c41971064.lua
@@ -2,7 +2,16 @@
 local s,id=GetID()
 function s.initial_effect(c)
 c:EnableReviveLimit()
-	Link.AddProcedure(c,nil,3,4,lcheck)
+	Link.AddProcedure(c,nil,2,4,lcheck)
+	--extra mat
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_FIELD)
+	e0:SetRange(LOCATION_EXTRA)
+	e0:SetCode(EFFECT_EXTRA_MATERIAL)
+	e0:SetProperty(EFFECT_FLAG_PLAYER_TARGET|EFFECT_FLAG_CANNOT_DISABLE|EFFECT_FLAG_SET_AVAILABLE)
+	e0:SetTargetRange(1,0)
+	e0:SetValue(s.extraval)
+	c:RegisterEffect(e0)
 	--damage conversion
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
@@ -26,6 +35,19 @@ c:EnableReviveLimit()
 	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e3:SetOperation(s.regop)
 	c:RegisterEffect(e3)
+end
+function s.lcheck(g,lc,sumtype,tp)
+	return g:IsExists(Card.IsSetCard,1,nil,0xf25,lc,sumtype,tp)
+end
+function s.extraval(chk,summon_type,e,...)
+	if chk==0 then
+		local tp,sc=...
+		if summon_type~=SUMMON_TYPE_LINK or sc~=e:GetHandler() then
+			return Group.CreateGroup()
+		else
+			return Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_PZONE,0,nil)
+		end
+	end
 end
 function s.regop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
@@ -66,7 +88,7 @@ function s.reptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return eg:IsExists(s.repfilter,1,nil,tp) and Duel.IsExistingMatchingCard(s.repcfilter,tp,LOCATION_GRAVE,0,2,nil) end
 	if Duel.SelectEffectYesNo(tp,e:GetHandler(),96) then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESREPLACE)
-		local g=Duel.SelectMatchingCard(tp,s.repcfilter,tp,LOCATION_GRAVE,0,3,3,nil)
+		local g=Duel.SelectMatchingCard(tp,s.repcfilter,tp,LOCATION_GRAVE,0,2,2,nil)
 		Duel.Remove(g,POS_FACEUP,REASON_EFFECT)
 		return true
 	else return false end
