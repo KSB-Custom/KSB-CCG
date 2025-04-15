b02ba3382a1ea1f8862eb6a288dd16e06eaf9e26
KSB-Custom <124850393+KSB-Custom@users.noreply.github.com>
2025-04-14 19:14:33 -0600

    Add files via upload

---
+++ script/c15220012.lua
@@ -0,0 +1,36 @@
+--Chris Super Speed
+local s,id=GetID()
+function s.initial_effect(c)
+	--Special Summon this card (from your hand)
+	local e0=Effect.CreateEffect(c)
+	e0:SetDescription(aux.Stringid(id,0))
+	e0:SetType(EFFECT_TYPE_FIELD)
+	e0:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
+	e0:SetCode(EFFECT_SPSUMMON_PROC)
+	e0:SetRange(LOCATION_HAND)
+	e0:SetCondition(s.spcon)
+	e0:SetTarget(s.sptg)
+	e0:SetOperation(s.spop)
+	c:RegisterEffect(e0)
+	
+end
+s.listed_series={0x1f17}
+function s.spconfilter(c)
+	return c:IsSetCard(0x1f17) and c:IsMonster() and c:IsAbleToDeckOrExtraAsCost()
+end
+function s.spcon(e,c)
+	if c==nil then return true end
+	local tp=e:GetHandlerPlayer()
+	return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.spconfilter,tp,LOCATION_ONFIELD,0,1,c)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,c)
+	local rg=Duel.GetMatchingGroup(s.spconfilter,tp,LOCATION_ONFIELD,0,c)
+	local g=aux.SelectUnselectGroup(rg,e,tp,1,1,nil,1,tp,HINTMSG_TODECK,nil,nil,true)
+	if #g>0 then
+		g:KeepAlive()
+		e:SetLabelObject(g)
+		return true
+	end
+	return false
+end
---
+++ script/c15220013.lua
@@ -0,0 +1,46 @@
+--Chris Twister Drive
+local s,id=GetID()
+function s.initial_effect(c)
+	--Special Summon this card (from your hand)
+	local e0=Effect.CreateEffect(c)
+	e0:SetDescription(aux.Stringid(id,0))
+	e0:SetType(EFFECT_TYPE_FIELD)
+	e0:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
+	e0:SetCode(EFFECT_SPSUMMON_PROC)
+	e0:SetRange(LOCATION_HAND)
+	e0:SetCondition(s.spcon)
+	e0:SetTarget(s.sptg)
+	e0:SetOperation(s.spop)
+	c:RegisterEffect(e0)
+end
+s.listed_series={0x1f17}
+function s.spconfilter(c)
+	return c:IsSetCard(0x1f17) and c:IsMonster() and c:IsAbleToDeckOrExtraAsCost()
+end
+function s.spcon(e,c)
+	if c==nil then return true end
+	local tp=e:GetHandlerPlayer()
+	return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.spconfilter,tp,LOCATION_ONFIELD,0,1,c)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,c)
+	local rg=Duel.GetMatchingGroup(s.spconfilter,tp,LOCATION_ONFIELD,0,c)
+	local g=aux.SelectUnselectGroup(rg,e,tp,1,1,nil,1,tp,HINTMSG_TODECK,nil,nil,true)
+	if #g>0 then
+		g:KeepAlive()
+		e:SetLabelObject(g)
+		return true
+	end
+	return false
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=e:GetLabelObject()
+	if not g then return end
+	if g:IsExists(Card.IsLocation,1,nil,LOCATION_HAND) then
+		Duel.ConfirmCards(1-tp,g)
+	else
+		Duel.HintSelection(g)
+	end
+	Duel.SendtoDeck(g,nil,SEQ_DECKSHUFFLE,REASON_COST)
+	g:DeleteGroup()
+end
