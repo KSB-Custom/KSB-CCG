815c062feea25d045c4f637426e7bce404aa5aa2
KSB-Custom <124850393+KSB-Custom@users.noreply.github.com>
2025-08-27 15:54:01 -0600

    Add files via upload

--- script/c25250112.lua
+++ script/c25250112.lua
@@ -17,7 +17,7 @@ function s.initial_effect(c)
 	--spsummon
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,0))
-	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetCategory(CATEGORY_DRAW)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetCountLimit(1)
